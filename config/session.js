6f5eee1acd295757c8824e0e93e4defa65487db4
MatchaDesu <matchadesu02@gmail.com>
2026-02-26 00:51:58 +0700

    change .env

--- config/session.js
+++ config/session.js
@@ -2,7 +2,7 @@ const session = require('express-session');
 
 module.exports = session({
   name: process.env.SESTION_NAME || 'whicha.sid',
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET || 'sessionKeyForTesting',
   resave: false,
   saveUninitialized: false,
   cookie: {
